f6479bd62b631eabae4620c47cb6fba4f51e2fa1
Probir Ghosh <probirghosh@outlook.com>
2026-09-09 12:57:19 +0600

    :fire: eslint configuration done

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import app from "./app";
 
-const app: Application = express();
-const port = 5000; // The port your express server will be running on.
+const bootstrap = () => {
+  try {
+    app.listen(5000, () => {
+      console.log(`Server is running on http://localhost:5000`);
+    });
+  } catch (err) {
+    console.error("Error occurred while bootstrapping the server:", err);
+  }
+};
 
-// Enable URL-encoded form data parsing
-app.use(express.urlencoded({ extended: true }));
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello,PH Healthcare Server!');
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+bootstrap();
